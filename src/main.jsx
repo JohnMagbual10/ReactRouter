89c3512d9e00dc350c4db1006b5317e75041fb41
John Magbual <jmagbual@Johns-Air.hsd1.ca.comcast.net>
2024-04-15 20:40:36 -0700

    navbar

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
+import { BrowserRouter, Link } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
-import ReactDOM from 'react-dom'; // Import ReactDOM
-
-import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
 
-createRoot(document.getElementById('root')).render( // Use createRoot directly
+createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
